29cc40ba1d419304a40c37e5c7af60a9dca4e59d
Kate G <h34rt8e4t@gmail.com>
2022-10-02 20:37:18 -0700

    renamed proj

--- project1/project.cpp
+++ project1/project.cpp
@@ -271,7 +271,7 @@ int main(int argc, char ** argv)
 {
     glutInit(&argc,argv);
     glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB|GLUT_DEPTH);
-    glutCreateWindow("PROJECT1. it's a guy");
+    glutCreateWindow("PROJECT1. the guy and his sick sad world");
     glutReshapeFunc(myReshape);
     glutDisplayFunc(Display);
     glutIdleFunc(Display);
